be97942bdd835945f6c05269212cbe48065710d3
SdxCoder <chsaadahmed93@gmail.com>
2020-02-23 22:46:51 +0500

    updated

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/index.html": "e046f1bebff6b79ecd0c981eb16ea32c",
+"/index.html": "95cc4cf58021bdacd65ab8a836c0f2e3",
 "/main.dart.js": "70f2a924a80764e67750a4608f8bc39b",
 "/manifest.json": "e59b1d454a7c4547eaec7989922d466d"
 };
