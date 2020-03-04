ed5c635caf0097ba208fcb00c75e4004620f501b
winwisely111 <winwisely111@gmail.com>
2020-03-04 23:22:33 +0500

    updated

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "dfa0869e20e516fd8ed06de68d9bfe6b",
-"/main.dart.js": "ba72dbab52befabec43fbb572391372f",
+"/main.dart.js": "209490d6c7598ecfda5ce7448a8d21ae",
 "/manifest.json": "e59b1d454a7c4547eaec7989922d466d"
 };
 
